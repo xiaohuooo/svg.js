2eb3ea48dd2720bbb8b2c4b0493941d483f98389
xiaohuooo <453650969@qq.com>
2023-04-24 09:42:56 +0800

    feat: container

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import Theme from "vitepress/theme";
 import "./styles/style.css";
 import { useComponents } from './useComponents'
-import { SVG } from '@svgdotjs/svg.js'
 export default {
   ...Theme,
   enhanceApp(ctx) {
